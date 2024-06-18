e3db735f0c31c268bf289598fe8c631dbe10de06
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-06-18 14:39:36 -0400

    created files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HTTP_project';
+  products : any;
+  constructor(private service: ProductsService) {}
+  ngOnInit(): void {
+    this.service.getProducts().subscribe((x)=>{this.products = x;});
+  }
 }
